17f5de8d3c5b73284553784fd2dacd2785a63f77
Clelo-567 <18318912888@163.com>
2020-08-20 11:43:26 +0800

    lst提交

--- js/api.js
+++ js/api.js
@@ -343,10 +343,10 @@ function jQueryWeb(url, data, complete) {
 		},
 		error: function(request, errorInfo) {
 			_mask.close();
-			alert("errorInfo = " + errorInfo);
-			// alert("进入error---\n" + "状态码："+request.status + "\n状态:"+request.readyState 
-			//    + "\n错误信息:"+request.statusText
-			//    +"\n返回响应信息："+request.responseText);
+			//alert("errorInfo = " + errorInfo);
+			alert("进入error---\n" + "状态码："+request.status + "\n状态:"+request.readyState 
+			   + "\n错误信息:"+request.statusText
+			   +"\n返回响应信息："+request.responseText);
 		}
 	});
 
@@ -406,31 +406,31 @@ function setUsrCode() {
 	var UsrCode = api_localStorageGet("code");
 	return ""+api_localStorageGet("name")+"("+UsrCode+")";
 }
-//打开新页面的方法
-function OpenWindow(id, url, extras) {
-	mui.openWindow({
-		id: id,
-		url: url,
-		styles: {
-			top: '0px', //新页面顶部位置 
-			bottom: '0px', //新页面底部位置 
-			//width: newpage - width, //新页面宽度，默认为100% 
-			//height: newpage - height, //新页面高度，默认为100% ...... 
-		},
-		extras: extras,
-		show: { //控制打开页面的类型
-			autoShow: true,
-			//页面loaded事件发生后自动显示，默认为true 
-			aniShow: 'slide-in-right', //页面显示动画，默认为”slide-in-right“；  页面出现的方式 左右上下
-			duration: '100' //页面动画持续时间，Android平台默认100毫秒，iOS平台默认200毫秒； 
-		},
-		waiting: { // 控制 弹出转圈框的信息
-			autoShow: true, //自动显示等待框，默认为true 
-			title: '加载中', //等待对话框上显示的提示内容 
-			options: {
-				width: '300px', //等待框背景区域宽度，默认根据内容自动计算合适宽度 
-				height: '100px', //等待框背景区域高度，默认根据内容自动计算合适高度 ...... 
-			}
-		}
-	});
-}
+// //打开新页面的方法
+// function OpenWindow(id, url, extras) {
+// 	mui.openWindow({
+// 		id: id,
+// 		url: url,
+// 		styles: {
+// 			top: '0px', //新页面顶部位置 
+// 			bottom: '0px', //新页面底部位置 
+// 			//width: newpage - width, //新页面宽度，默认为100% 
+// 			//height: newpage - height, //新页面高度，默认为100% ...... 
+// 		},
+// 		extras: extras,
+// 		show: { //控制打开页面的类型
+// 			autoShow: true,
+// 			//页面loaded事件发生后自动显示，默认为true 
+// 			aniShow: 'slide-in-right', //页面显示动画，默认为”slide-in-right“；  页面出现的方式 左右上下
+// 			duration: '100' //页面动画持续时间，Android平台默认100毫秒，iOS平台默认200毫秒； 
+// 		},
+// 		waiting: { // 控制 弹出转圈框的信息
+// 			autoShow: true, //自动显示等待框，默认为true 
+// 			title: '加载中', //等待对话框上显示的提示内容 
+// 			options: {
+// 				width: '300px', //等待框背景区域宽度，默认根据内容自动计算合适宽度 
+// 				height: '100px', //等待框背景区域高度，默认根据内容自动计算合适高度 ...... 
+// 			}
+// 		}
+// 	});
+// }
